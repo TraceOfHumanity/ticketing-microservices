c0de08ac9c4cd735c9eed98330224f4d30286bd7
Alexander_Ovchar <azalexxx02@gmail.com>
2025-08-07 00:00:18 +0300

    feat(auth): add tests for current user endpoint and implement signin helper function

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -3,17 +3,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("responde with details about the current user", async () => {
-  // const cookie = await global.signin();
-
-  const authResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
-
-  const cookie = authResponse.get("Set-Cookie");
+  const cookie = await global.signin();
 
   if (!cookie) {
     throw new Error("Cookie not set after signup");
@@ -26,4 +16,13 @@ it("responde with details about the current user", async () => {
     .expect(200);
 
   expect(response.body.currentUser.email).toEqual("test@test.com");
+});
+
+it("responde with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(401);
+
+  expect(response.body.currentUser).toEqual(undefined);
 });
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -2,6 +2,11 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { app } from "../app";
 import { beforeAll, beforeEach, afterAll } from "@jest/globals";
+import request from "supertest";
+
+declare global {
+  var signin: () => Promise<string[] | undefined>;
+}
 
 let mongo: any;
 
@@ -30,3 +35,17 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
