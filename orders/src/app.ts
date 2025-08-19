ec48adaebfb6565f421af8d75d6cfd451830ba07
Alexander_Ovchar <azalexxx02@gmail.com>
2025-08-19 21:42:35 +0300

    feat(orders): add routes for order management in the orders service

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,11 +2,11 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
 import { errorHandler, NotFoundError, currentUser } from "@thticketsms/common";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+import { newOrderRouter } from "./routers/new";
+import { showOrderRouter } from "./routers/show";
+import { indexOrderRouter } from "./routers/index";
+import { deleteOrderRouter } from "./routers/delete";
 
 const app = express();
 app.set("trust proxy", true);
@@ -20,10 +20,10 @@ app.use(
 
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
