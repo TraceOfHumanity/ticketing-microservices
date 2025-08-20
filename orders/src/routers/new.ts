958cecc503d9a5c13f70b41eb58f6c866ff4651c
Alexander_Ovchar <azalexxx02@gmail.com>
2025-08-20 19:25:19 +0300

    feat(orders): update @thticketsms/common dependency and add validation for order creation

--- orders/src/routers/new.ts
+++ orders/src/routers/new.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@thticketsms/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/orders", (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .isMongoId()
+      .withMessage("Ticket ID is required"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send({});
+  },
+);
 
 export { router as newOrderRouter };
